06601845673fe3da8fa65830740598f90eda54ef
Mitchell <mitch.ohara-wild@monash.edu>
2019-04-24 14:42:25 +1000

    Updated tsfresh_features to work with new features()

--- R/extract.R
+++ R/extract.R
@@ -1,9 +1,10 @@
 #' Extract tsfresh features
 #'
 #' @param x A vector (must be sorted in time order)
+#' @param ... Unused.
 #'
 #' @export
-tsfresh_features <- function(x){
+tsfresh_features <- function(x, ...){
   tsfresh_available()
   tsfresh$extract_features(data.frame(x = x, grp = "Unused"), column_id = "grp",
                            column_value = "x", disable_progressbar = TRUE)
